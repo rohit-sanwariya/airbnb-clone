bc0e5af55a8e1212a99abb857e78cacef0cf8fde
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2025-04-16 18:45:36 +0530

    removed unused entry component.

--- airbnb-clone-ng/apps/airbnb-clone-ng/src/app/app.component.ts
+++ airbnb-clone-ng/apps/airbnb-clone-ng/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'airbnb-clone-ng';
+  
 }
